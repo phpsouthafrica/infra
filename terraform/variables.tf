8a59fb817ff7ebefbac8f020ad38124c63120cd5
Tim Haak <tim@haak.co>
2021-03-24 16:46:49 +0200

    Add example of how to set token.
    Also url to look at later to do shared state with out committing to github

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,10 @@
-# Set the variable value in *.tfvars file
-# or using -var="do_token=..." CLI option
+// TODO
+// https://www.digitalocean.com/community/tutorials/how-to-use-terraform-within-your-team
+
+# Generate an api key then
+# export DIGITALOCEAN_TOKEN='MY_TOKEN'
+# export TF_VAR_do_token='MY_TOKEN'
+
 variable "do_token" {}
 
 variable region {
